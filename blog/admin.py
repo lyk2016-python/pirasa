c398b0bd6da6ab838900e72eea5d16fd39fff3b6
Fatih Erikli <fatiherikli@gmail.com>
2016-08-13 15:41:52 +0300

    Implement the initial version of demo app

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from blog.models import Post
+
+admin.site.register(Post)
+
---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    is_published = models.BooleanField()
+
+    def __str__(self):
+        return self.title
